2c1ace85e08c82a4ff5779e8d0625fc466637c50
Richard Cobbe <rcobbe@rcobbe.net>
2017-12-26 08:17:36 -0800

    Add module comment to list.js

--- list.js
+++ list.js
@@ -1,5 +1,34 @@
 "use strict";
 
+/************************************************************************
+ * 
+ * Inductive list type.
+ * 
+ * Exports:
+ *   empty :: List<a>
+ *   cons :: a List<a> -> List<a>
+ *   isEmpty :: List<a> -> Bool
+ *   fromArray :: Array<a> -> List<a>
+ *   toArray :: List<a> -> Array<a>
+ * 
+ * List<a> properties:
+ *   car: a  if list is not empty
+ *   cdr: list<a>  if list is not empty
+ *   map: (a -> b) -> List<b>
+ * 
+ * List<a> implements the Iterable protocol.
+ * 
+ * XXX I've implemented the toString method on the List objects to return a
+ * human-readable form of the list.  I'm a little nervous about this, since
+ * I'm not sure when toString gets called or if other code depends on the
+ * default toString output.  If this turns out to be an issue, fall back on
+ * the "show" mechanism: rename toString to show below, and define a new
+ * module that exports a single top-level function show(x), which calls
+ * x.show() if it exists & calls x.toString() otherwise.  Then replace calls
+ * to toString below with calls to show.
+ *
+ ***********************************************************************/
+
 const empty = { 
     [Symbol.iterator]: function iterator() { return mkIter(this); },
     toString: function toString() { return "()"; },
