1593fca31c5591510e743bc49fd7613f384f21f2
Richard Cobbe <rcobbe@rcobbe.net>
2017-12-29 08:54:28 -0800

    Add module defining Pair type

---
+++ pair.js
@@ -0,0 +1,36 @@
+"use strict";
+
+/******************************************************************************
+ * 
+ * Defines the type Pair<a, b>.
+ * 
+ * Exports:
+ *   pair :: a b -> Pair<a, b>
+ *   isPair :: c -> Boolean
+ * 
+ * Properties of Pair<a, b>
+ *   fst :: a
+ *   snd :: b
+ *   toString :: () -> String
+ *   equal :: c -> Boolean
+ *
+ *******************************************************************************/
+
+var eq = require("./equal");
+
+function pair(x, y) {
+    return {
+        get constructor() { return pair; },
+        get fst() { return x; },
+        get snd() { return y; },
+        toString() {
+            return "(" + this.fst.toString() + ", " + this.snd.toString() + ")";
+        },
+        equal(rhs) {
+            return rhs.constructor === pair && eq.equal(this.fst, rhs.fst) && eq.equal(this.snd, rhs.snd);
+        },
+    };
+}
+
+exports.pair = pair;
+exports.isPair = function isPair(x) { return x !== null && x !== undefined && x.constructor === pair; };
