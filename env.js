f33bb3c5472c0196154d5f39e1357fbbf5c90171
Richard Cobbe <rcobbe@rcobbe.net>
2018-02-02 15:05:25 -0800

    Parameterize Env over key equality; add bind, bindLots

--- env.js
+++ env.js
@@ -2,11 +2,14 @@
 
 /************************************************************
  *
- * Defines an Env<k, v> type.
+ * Defines an Env<k, v> type.  Key identity is defined in terms of the === operator.
  *
  * Exports:
- *   empty :: Env<k, v>
+ *   empty :: (k k -> Boolean) -> Env<k, v>
  *   isEnv :: a -> Boolean
+ *   bind :: Env<k, Addr> Store<v> k v -> { env :: Env<k, Addr>, store :: Store<v> }
+ *   bindLots :: Env<k, Addr> Store<v> Iterable<{key :: k, value :: v}> -> {env :: Env<k, Addr>, store :: Store<v>}
+ *     If a key appears multiple times in the argument, the leftmost binding takes precedence.
  *
  * Env<k, v> properties:
  *   lookup :: k -> v          throws if not bound
@@ -19,14 +22,16 @@
 var list = require("./list");
 
 class Env {
-    // bindings :: List<{key :: k, value :: v}>
-    constructor(bindings) {
+    // _eq :: k k -> Boolean
+    // _bindings :: List<{key :: k, value :: v}>
+    constructor(eq, bindings) {
+        this._eq = eq;
         this._bindings = bindings;
     }
 
     lookup(key) {
         for (let {key: k, value: v} of this._bindings) {
-            if (k === key) {
+            if (this._eq(k, key)) {
                 return v;
             }
         }
@@ -34,16 +39,44 @@ class Env {
     }
 
     extend(key, value) {
-        return new Env(list.cons({key: key, value: value}, this._bindings));
+        return new Env(this._eq, list.cons({key: key, value: value}, this._bindings));
     }
 
     extendLots(keyValuePairs) {
-        return keyValuePairs.reduceRight(
-            (env, {key, value}) => env.extend(key, value),
-            this
-        );
+        let kvp;
+        if (Array.isArray(keyValuePairs)) {
+            kvp = list.fromArray(keyValuePairs);
+        } else if (list.isList(keyValuePairs)) {
+            kvp = keyValuePairs;
+        } else {
+            throw new TypeError("Env.extendLots: expected list or array; got " + keyValuePairs);
+        }
+        return new Env(this._eq, kvp.append(this._bindings));
+    }
+
+    toString() {
+        let result = "Env:\n";
+        for (let {key, value} of this._bindings) {
+            result += "  " + key.toString() + " -> " + value.toString() + "\n";
+        }
+        return result;
     }
 }
 
-exports.empty = new Env(list.empty);
+exports.empty = function empty(eq) { return new Env(eq, list.empty); };
 exports.isEnv = function isEnv(x) { return x !== null && x !== undefined && x.constructor === Env; };
+
+function bind(env, store, k, v) {
+    let {store: newSt, addr} = store.alloc(v);
+    return {env: env.extend(k, addr), store: newSt};
+}
+exports.bind = bind;
+
+function bindLots(env, store, keyValuePairs) {
+    let initAccum = {env: env, store: store};
+    return keyValuePairs.reduceRight(
+        ({env, store}, {key, value}) => bind(env, store, key, value),
+        initAccum
+    );
+}
+exports.bindLots = bindLots;
