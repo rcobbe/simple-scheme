761f09bc28450d43d92a4db183216e23c88fa37e
Richard Cobbe <rcobbe@rcobbe.net>
2018-02-08 07:06:12 -0800

    Change value.isValue to rely on prop w/ symbolic name
    Makes it harder, tho not impossible, to spoof.

--- value.js
+++ value.js
@@ -5,31 +5,26 @@ var env = require("./env");
 var exp = require("./exp");
 var k = require("./continuation");
 
+// Tag used to identify values that can't (easily) be spoofed
+const valueTag = Symbol("valueTag");
+
 function mkPred(tag) {
     console.assert(typeof(tag) === "string");
-    return obj => (
-        obj !== null &&
-        obj !== undefined &&
-        Object.prototype.hasOwnProperty.call(obj, "valueType") &&
-        obj.valueType === tag
-    );
+    return obj => (isValue(obj) && obj.valueType === tag);
 }
-
-const valueTags =
-    [ "number", "string", "symbol", "boolean", "void", "pair", "closure", "continuation", "undefined" ];
 
 function isValue(x) {
     return (
         x !== null &&
         x !== undefined &&
-        Object.prototype.hasOwnProperty.call(x, "valueType") &&
-        Array.prototype.includes.call(valueTags, x.valueTag)
+        Object.prototype.hasOwnProperty.call(x, valueTag)
     );
 }
 
 exports.numVal = function numVal(n) {
     console.assert(typeof(n) === "number");
     return {
+        [valueTag]: valueTag,
         valueType: "number",
         value: n,
         equal(rhs) { return this.valueType === rhs.valueType && this.value === rhs.value; }
@@ -40,6 +35,7 @@ exports.isNumVal = mkPred("number");
 exports.strVal = function strVal(s) {
     console.assert(typeof(s) === "string");
     return {
+        [valueTag]: valueTag,
         valueType: "string",
         value: s,
         equal(rhs) { return this.valueType === rhs.valueType && this.value === rhs.value; }
@@ -50,6 +46,7 @@ exports.isStrVal = mkPred("string");
 exports.symVal = function symVal(s) {
     console.assert(typeof(s) === "string");
     return {
+        [valueTag]: valueTag,
         valueType: "symbol",
         value: s,
         equal(rhs) { return this.valueType === rhs.valueType && this.value === rhs.value; }
@@ -59,6 +56,7 @@ exports.isSymVal = mkPred("symbol");
 
 exports.nullVal = function nullVal() {
     return {
+        [valueTag]: valueTag,
         valueType: "null",
         equal(rhs) { return this.valueType === rhs.valueType; }
     };
@@ -68,6 +66,7 @@ exports.isNullVal = mkPred("null");
 exports.boolVal = function boolVal(b) {
     console.assert(typeof(b) === "boolean");
     return {
+        [valueTag]: valueTag,
         valueType: "boolean",
         value: b,
         equal(rhs) { return this.valueType === rhs.valueType && this.value === rhs.value; }
@@ -77,6 +76,7 @@ exports.isBoolVal = mkPred("boolean");
 
 exports.voidVal = function voidVal() {
     return {
+        [valueTag]: valueTag,
         valueType: "void",
         equal(rhs) { return this.valueType === rhs.valueType; }
     };
@@ -87,6 +87,7 @@ exports.pairVal = function pairVal(car, cdr) {
     console.assert(isValue(car));
     console.assert(isValue(cdr));
     return {
+        [valueTag]: valueTag,
         valueType: "pair",
         car: car,
         cdr: cdr,
@@ -102,6 +103,7 @@ exports.closureVal = function closureVal(rho, formals, body) {
     for (let x in formals) { console.assert(typeof(x) === "string"); }
     console.assert(exp.isExp(body));
     return {
+        [valueTag]: valueTag,
         valueType: "closure",
         env: rho,
         formals: formals,
@@ -119,6 +121,7 @@ exports.isClosureVal = mkPred("closure");
 exports.continuationVal = function continuationVal(kappa) {
     console.assert(k.isContinuation(kappa));
     return {
+        [valueTag]: valueTag,
         valueType: "continuation",
         k: kappa,
         equal(rhs) {
@@ -130,6 +133,7 @@ exports.isContinuationVal = mkPred("continuation");
 
 exports.undefinedVal = function undefinedVal() {
     return {
+        [valueTag]: valueTag,
         valueType: "undefined",
         equal(rhs) { return this.valueType === rhs.valueType; }
     };
