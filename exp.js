037f464d16d89fd67ced6858283b20a4e840f9f3
Richard Cobbe <rcobbe@rcobbe.net>
2018-02-09 18:07:36 -0800

    Finished definition of Exp ADT

--- exp.js
+++ exp.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var iterUtils = require("./iterable-utils");
+var list = require("./list");
 
 // Tag that can't be easily spoofed to identify exp AST nodes
 const expTag = Symbol("expTag");
@@ -69,8 +70,7 @@ exports.isId = mkPred("id");
 
 exports.lambda = function lambda(args, body) {
     console.assert(iterUtils.isIterable(args));
-    console.assert(iterUtils.andmap(x => typeof(x) === "string", body));
-    for (let x of args) { console.assert(typeof(x) === "string"); }
+    console.assert(iterUtils.andmap(x => typeof(x) === "string", args));
     console.assert(isExp(body));
     return {
         [expTag]: expTag,
@@ -81,7 +81,116 @@ exports.lambda = function lambda(args, body) {
 };
 exports.isLambda = mkPred("lambda");
 
-/*
 exports.letExp = function letExp(xs, rhss, body) {
-    console.assert(Array.isArray(xs) || list.isList(xs));
-*/
+    console.assert(iterUtils.isIterable(xs));
+    console.assert(iterUtils.andmap(x => typeof(x) === "string"), xs);
+    console.assert(iterUtils.isIterable(rhss));
+    console.assert(iterUtils.andmap(isExp, rhss));
+    console.assert(iterUtils.length(xs) === iterUtils.length(rhss));
+    console.assert(isExp(body));
+    return {
+        [expTag]: expTag,
+        expType: "let",
+        xs: xs,
+        rhss: rhss,
+        body: body
+    };
+};
+exports.isLetExp = mkPred("let");
+
+exports.letrecExp = function letrecExp(xs, rhss, body) {
+    console.assert(iterUtils.isIterable(xs));
+    console.assert(iterUtils.andmap(x => typeof(x) === "string"), xs);
+    console.assert(iterUtils.isIterable(rhss));
+    console.assert(iterUtils.andmap(isExp, rhss));
+    console.assert(iterUtils.length(xs) === iterUtils.length(rhss));
+    console.assert(isExp(body));
+    return {
+        [expTag]: expTag,
+        expType: "letrec",
+        xs: xs,
+        rhss: rhss,
+        body: body
+    };
+};
+exports.isLetrecExp = mkPred("letrec");
+
+exports.letccExp = function letccExp(x, body) {
+    console.assert(typeof(x) === "string");
+    console.assert(isExp(body));
+    return {
+        [expTag]: expTag,
+        expType: "let/cc",
+        x: x,
+        body: body
+    };
+};
+exports.isLetccExp = mkPred("let/cc");
+
+exports.setExp = function setExp(x, rhs) {
+    console.assert(typeof(x) === "string");
+    console.assert(isExp(rhs));
+    return {
+        [expTag]: expTag,
+        expType: "set!",
+        x: x,
+        rhs: rhs
+    };
+};
+exports.isSetExp = mkPred("set!");
+
+exports.ifExp = function ifExp(test, consequent, alternative) {
+    console.assert(isExp(test));
+    console.assert(isExp(consequent));
+    console.assert(isExp(alternative));
+    return {
+        [expTag]: expTag,
+        expType: "if",
+        test: test,
+        consequent: consequent,
+        alternative: alternative
+    };
+};
+exports.isIfExp = mkPred("if");
+
+exports.andExp = function andExp(...args) {
+    console.assert(iterUtils.andmap(isExp, args));
+    return {
+        [expTag]: expTag,
+        expType: "and",
+        args: list.fromIterable(args)
+    };
+};
+exports.isAndExp = mkPred("and");
+
+exports.orExp = function orExp(...args) {
+    console.assert(iterUtils.andmap(isExp, args));
+    return {
+        [expTag]: expTag,
+        expType: "or",
+        args: list.fromIterable(args)
+    };
+};
+exports.isOrExp = mkPred("or");
+
+exports.beginExp = function beginExp(...exps) {
+    console.assert(iterUtils.andmap(isExp, exps));
+    return {
+        [expTag]: expTag,
+        expType: "begin",
+        bodyExps: list.fromIterable(exps)
+    };
+};
+exports.isBeginExp = mkPred("begin");
+
+exports.appExp = function appExp(rator, ...rands) {
+    console.assert(isExp(rator));
+    console.assert(iterUtils.andmap(isExp, rands));
+    return {
+        [expTag]: expTag,
+        expType: "app",
+        rator: rator,
+        rands: list.fromIterable(rands)
+    };
+};
+exports.isAppExp = mkPred("app");
